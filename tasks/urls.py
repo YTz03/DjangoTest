66236b4a83f29e40ffe57925d0b8ed79cd8f716f
Jonathan <yonny9922@gmail.com>
2025-08-29 18:34:57 +0300

    new app

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
-    path('update/', views.update_tasks, name='update_tasks'),
+    path('add/', views.add_task, name='add_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from .form import TaskForm
 
 
+# Create your views here.
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
-# Create your views here.
- 
-def update_tasks(request):
-    if request.method == 'POST':
-        for key, value in request.POST.items():
-            if key.startswith('title_'):
-                task_id = key.split('_')[1]
-                try:
-                    task = Task.objects.get(id=task_id)
-                    task.title = value
-                    # עדכון הסטטוס
-                    completed_key = f'completed_{task_id}'
-                    task.completed = completed_key in request.POST
-                    task.save()
-                except Task.DoesNotExist:
-                    pass
-    return redirect('task_list')
+
+
+def add_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/add_task.html', {'form': form})
